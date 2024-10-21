9e79347da6ce2c0773fe630d5c0858f572b3cf0c
Gautam1479 <gautamyd786@gmail.com>
2024-10-21 16:48:56 +0530

    Delete Requirements.py

--- Requirements.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import streamlit as st
-import joblib  # pip install joblib
-from sklearn.preprocessing import StandardScaler as ss
-import pandas as pd
-import pickle
-import joblib
