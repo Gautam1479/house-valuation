953e766fef48f7a7ced5a9a19d899e4c1c42930f
Gautam1479 <gautamyd786@gmail.com>
2024-10-24 13:53:14 +0530

    changes

--- house.py
+++ house.py
@@ -7,23 +7,27 @@ import pickle
 import joblib
 # 
 # Streamlit app configuration
-st.set_page_config("House Valuation")
+st.set_page_config("House Valuation",page_icon="	:house_with_garden:")
 st.title("House Price Calculator")
+col1,col2=st.columns(2)
 
 # Input fields (they return strings, so we need to convert them to numeric types later)
-val1 = st.number_input("Area", placeholder="Enter the area size in square feet")
-val2 = st.number_input("Bedrooms", placeholder="Enter the number of bedrooms")
-val3 = st.number_input("Bathrooms", placeholder="Enter the number of bathrooms")
-val4 = st.number_input("Stories", placeholder="Enter the number of stories")
-val5 = st.number_input("Parking", placeholder="Enter the number of extra parking")
+with col1:
+ val1 = st.number_input("Area", placeholder="Enter the area size in square feet")
+ val2 = st.number_input("Bedrooms", placeholder="Enter the number of bedrooms")
+ val3 = st.number_input("Bathrooms", placeholder="Enter the number of bathrooms")
+ val4 = st.number_input("Stories", placeholder="Enter the number of stories")
+ val5 = st.number_input("Parking", placeholder="Enter the number of extra parking")
+has_furniture = st.radio('Furnished or not?', ['Furnished', 'Semi-Furnished', 'Unfurnished'])
 
 # Radio buttons for Yes/No features
-has_mainroad = st.radio('Has Mainroad?', ['Yes', 'No'])
-has_guestroom = st.radio('Has Guestroom?', ['Yes', 'No'])
-has_basement = st.radio('Has Basement?', ['Yes', 'No'])
-has_aircondi = st.radio('Has Airconditioning?', ['Yes', 'No'])
-has_prefarea = st.radio('Location in Preferred Area?', ['Yes', 'No'])
-has_furniture = st.radio('Furnished or not?', ['Furnished', 'Semi-Furnished', 'Unfurnished'])
+with col2:
+ has_mainroad = st.radio('Has Mainroad?', ['Yes', 'No'])
+ has_guestroom = st.radio('Has Guestroom?', ['Yes', 'No'])
+ has_basement = st.radio('Has Basement?', ['Yes', 'No'])
+ has_aircondi = st.radio('Has Airconditioning?', ['Yes', 'No'])
+ has_prefarea = st.radio('Location in Preferred Area?', ['Yes', 'No'])
+
 has_mainroad = 1 if has_mainroad == 'Yes' else 0
 has_guestroom = 1 if has_guestroom == 'Yes' else 0
 has_basement = 1 if has_basement == 'Yes' else 0
@@ -49,8 +53,6 @@ model=joblib.load('Housing.pkl')
 if st.button('Predict'):
     scaled_vals = scaler.transform([[val1, val2, val3, val4, val5]])
     scaled_vals = scaled_vals.flatten()
-# Create the feature array by concatenating flattened scaled_vals and other features
     features = np.array([*scaled_vals, has_mainroad, has_guestroom, has_basement, has_aircondi, has_prefarea,semi_furniture, unfurnished])
-    # st.write(features)
     prediction = model.predict([features])
     st.write(f"Predicted House Price: ${prediction[0]:,.2f}")   
